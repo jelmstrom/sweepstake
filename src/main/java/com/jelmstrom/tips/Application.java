da03358a39cd30dc563bafea9f40972dc635b761
Johan Elmström <johan.elmstrom@gmail.com>
2014-06-16 21:35:17 +0200

    #7 setting encoding in servlet filter

--- src/main/java/com/jelmstrom/tips/Application.java
+++ src/main/java/com/jelmstrom/tips/Application.java
@@ -3,10 +3,16 @@ package com.jelmstrom.tips;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import java.util.EnumSet;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,6 +21,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        ConfigurationLoader.initialiseData(SweepstakeController.context);
 
     }
 
@@ -25,4 +32,15 @@ public class Application {
         return new TomcatEmbeddedServletContainerFactory(8080);
     }
 
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return (ServletContext servletContext) -> {
+            final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+            characterEncodingFilter.setEncoding("UTF-8");
+            characterEncodingFilter.setForceEncoding(false);
+
+        servletContext.addFilter("characterEncodingFilter", characterEncodingFilter)
+                    .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
+        };
+    }
 }
